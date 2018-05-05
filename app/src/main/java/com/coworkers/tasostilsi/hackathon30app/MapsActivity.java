e9efd752ac1b539a039a9756597ed5b785c582b3
tasostilsi <tasostilsi@hotmail.com>
2018-05-05 16:21:47 +0300

    going to Part B with not dynamically locations given

--- app/src/main/java/com/coworkers/tasostilsi/hackathon30app/MapsActivity.java
+++ app/src/main/java/com/coworkers/tasostilsi/hackathon30app/MapsActivity.java
@@ -1,13 +1,13 @@
 package com.coworkers.tasostilsi.hackathon30app;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.androidnetworking.AndroidNetworking;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -15,25 +15,70 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
-import java.util.ArrayList;
+import libs.mjn.prettydialog.PrettyDialog;
+import libs.mjn.prettydialog.PrettyDialogCallback;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    private NetworkAvailability network;
+    private FrameLayout progressBar;
+    private PrettyDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         AndroidNetworking.initialize(getApplicationContext());
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
 
-        mapFragment.getMapAsync(this);
+        progressBar = findViewById(R.id.progressBar);
+
+        network = new NetworkAvailability(this);
+        if (network.isNetworkWorks()) {
+            progressBar.setVisibility(View.GONE);
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+
+            mapFragment.getMapAsync(this);
+        } else {
+            dialog = new PrettyDialog(this);
+            dialog.setTitle(getString(R.string.app_name))
+                    .setMessage(getString(R.string.checkForNetworkMessage))
+                    .addButton(getString(R.string.retry), android.R.color.white, android.R.color.holo_green_light, new PrettyDialogCallback() {
+                        @Override
+                        public void onClick() {
+                            recreate();
+                            dialog.dismiss();
+                        }
+                    })
+                    .setAnimationEnabled(true)
+                    .show();
+        }
     }
 
+    @Override
+    public void onBackPressed() {
+
+        dialog = new PrettyDialog(this);
+        dialog.setTitle(getString(R.string.app_name))
+                .setMessage(getString(R.string.exitTheAppMessage))
+                .addButton(getString(android.R.string.ok), android.R.color.white, android.R.color.holo_red_light, new PrettyDialogCallback() {
+                    @Override
+                    public void onClick() {
+                        MapsActivity.super.onBackPressed();
+                        dialog.dismiss();
+                    }
+                })
+                .addButton(getString(android.R.string.cancel), android.R.color.white, android.R.color.holo_blue_light, new PrettyDialogCallback() {
+                    @Override
+                    public void onClick() {
+                        dialog.dismiss();
+                    }
+                })
+                .setAnimationEnabled(true)
+                .show();
+    }
 
     /**
      * Manipulates the map once available.
@@ -74,14 +119,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng skoutari = new LatLng(41.016434, 23.434656);
         mMap.addMarker(new MarkerOptions().position(skoutari).title("skoutari"));*/
 
-       getTheLocationsAndAddMarkersOnMap(googleMap);
+        getTheLocationsAndAddMarkersOnMap(googleMap);
 
 
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(41.092083, 23.541016), 7.0f));
     }
 
-    private void getTheLocationsAndAddMarkersOnMap(GoogleMap mMap){
+    private void getTheLocationsAndAddMarkersOnMap(GoogleMap mMap) {
         LatLng serres = new LatLng(41.092083, 23.541016);
         mMap.addMarker(new MarkerOptions().position(serres).title("Serres"));
 
@@ -112,28 +157,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng peponia = new LatLng(40.988154, 23.516756);
         mMap.addMarker(new MarkerOptions().position(peponia).title("peponia"));
 
-        addPolyline(mMap,serres,provatas);
-        addPolyline(mMap,serres,kmitrousi);
-        addPolyline(mMap,serres,skoutari);
-        addPolyline(mMap,provatas,akamila);
-        addPolyline(mMap,kmitrousi,akamila);
-        addPolyline(mMap,kmitrousi,kkamila);
-        addPolyline(mMap,kmitrousi,koumaria);
-        addPolyline(mMap,skoutari,kkamila);
-        addPolyline(mMap,skoutari,ageleni);
-        addPolyline(mMap,skoutari,peponia);
-        addPolyline(mMap,akamila,koumaria);
-        addPolyline(mMap,kkamila,koumaria);
-        addPolyline(mMap,koumaria,adelfiko);
-        addPolyline(mMap,ageleni,peponia);
-        addPolyline(mMap,peponia,adelfiko);
+        addPolyline(mMap, serres, provatas);
+        addPolyline(mMap, serres, kmitrousi);
+        addPolyline(mMap, serres, skoutari);
+        addPolyline(mMap, provatas, akamila);
+        addPolyline(mMap, kmitrousi, akamila);
+        addPolyline(mMap, kmitrousi, kkamila);
+        addPolyline(mMap, kmitrousi, koumaria);
+        addPolyline(mMap, skoutari, kkamila);
+        addPolyline(mMap, skoutari, ageleni);
+        addPolyline(mMap, skoutari, peponia);
+        addPolyline(mMap, akamila, koumaria);
+        addPolyline(mMap, kkamila, koumaria);
+        addPolyline(mMap, koumaria, adelfiko);
+        addPolyline(mMap, ageleni, peponia);
+        addPolyline(mMap, peponia, adelfiko);
     }
 
-    private void addPolyline(GoogleMap googleMap, LatLng start, LatLng end){
+    private void addPolyline(GoogleMap googleMap, LatLng start, LatLng end) {
         Polyline polyline = googleMap.addPolyline(new PolylineOptions()
                 .clickable(true)
-                .add(start,end));
+                .add(start, end));
 
 
     }
+
+
 }
