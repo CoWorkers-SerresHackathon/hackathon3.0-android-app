e448aeb92eb8b04a9938d462572d78a104ed8075
tasostilsi <tasostilsi@hotmail.com>
2018-05-05 14:41:14 +0300

    map is ready but not dynamically at this time

---
+++ README.md
@@ -0,0 +1,18 @@
+# Hackathon3.0 App
+A Project created for Serres Hackathon 2018!!!
+
+## About
+https://github.com/serrestech/hackathon/tree/gh-pages/pages/topic
+http://hackathon.serrestech.gr
+
+## Requirements
+
+## Install
+
+## Usage
+
+## Live demo
+
+## License
+
+[MIT](LICENSE)
--- app/src/main/java/com/coworkers/tasostilsi/hackathon30app/MapsActivity.java
+++ app/src/main/java/com/coworkers/tasostilsi/hackathon30app/MapsActivity.java
@@ -12,6 +12,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -45,17 +49,91 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng serres = new LatLng(41.092083, 23.541016);
+       /* LatLng serres = new LatLng(41.092083, 23.541016);
         mMap.addMarker(new MarkerOptions().position(serres).title("Serres"));
 
-        LatLng provatas = new LatLng(41.068238,  23.390686);
+        LatLng provatas = new LatLng(41.068238, 23.390686);
         mMap.addMarker(new MarkerOptions().position(provatas).title("provatas"));
 
-        LatLng akamila = new LatLng(41.058320,  23.424134);
+        LatLng akamila = new LatLng(41.058320, 23.424134);
         mMap.addMarker(new MarkerOptions().position(akamila).title("akamila"));
+
+        Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
+                .clickable(true)
+                .add(serres, provatas, akamila));
+
+        LatLng kkamila = new LatLng(41.020431, 23.483293);
+        mMap.addMarker(new MarkerOptions().position(kkamila).title("kkamila"));
+
+        LatLng kmitrousi = new LatLng(41.058680, 23.457547);
+        mMap.addMarker(new MarkerOptions().position(kmitrousi).title("kmitrousi"));
+
+        LatLng koumaria = new LatLng(41.016434, 23.434656);
+        mMap.addMarker(new MarkerOptions().position(koumaria).title("koumaria"));
+
+        LatLng skoutari = new LatLng(41.016434, 23.434656);
+        mMap.addMarker(new MarkerOptions().position(skoutari).title("skoutari"));*/
+
+       getTheLocationsAndAddMarkersOnMap(googleMap);
 
 
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(serres, 7.0f));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(41.092083, 23.541016), 7.0f));
+    }
+
+    private void getTheLocationsAndAddMarkersOnMap(GoogleMap mMap){
+        LatLng serres = new LatLng(41.092083, 23.541016);
+        mMap.addMarker(new MarkerOptions().position(serres).title("Serres"));
+
+        LatLng provatas = new LatLng(41.068238, 23.390686);
+        mMap.addMarker(new MarkerOptions().position(provatas).title("provatas"));
+
+        LatLng akamila = new LatLng(41.058320, 23.424134);
+        mMap.addMarker(new MarkerOptions().position(akamila).title("akamila"));
+
+        LatLng kkamila = new LatLng(41.020431, 23.483293);
+        mMap.addMarker(new MarkerOptions().position(kkamila).title("kkamila"));
+
+        LatLng kmitrousi = new LatLng(41.058680, 23.457547);
+        mMap.addMarker(new MarkerOptions().position(kmitrousi).title("kmitrousi"));
+
+        LatLng koumaria = new LatLng(41.016434, 23.434656);
+        mMap.addMarker(new MarkerOptions().position(koumaria).title("koumaria"));
+
+        LatLng skoutari = new LatLng(41.020032, 23.520701);
+        mMap.addMarker(new MarkerOptions().position(skoutari).title("skoutari"));
+
+        LatLng adelfiko = new LatLng(41.014645, 23.457354);
+        mMap.addMarker(new MarkerOptions().position(adelfiko).title("adelfiko"));
+
+        LatLng ageleni = new LatLng(41.003545, 23.559196);
+        mMap.addMarker(new MarkerOptions().position(ageleni).title("ageleni"));
+
+        LatLng peponia = new LatLng(40.988154, 23.516756);
+        mMap.addMarker(new MarkerOptions().position(peponia).title("peponia"));
+
+        addPolyline(mMap,serres,provatas);
+        addPolyline(mMap,serres,kmitrousi);
+        addPolyline(mMap,serres,skoutari);
+        addPolyline(mMap,provatas,akamila);
+        addPolyline(mMap,kmitrousi,akamila);
+        addPolyline(mMap,kmitrousi,kkamila);
+        addPolyline(mMap,kmitrousi,koumaria);
+        addPolyline(mMap,skoutari,kkamila);
+        addPolyline(mMap,skoutari,ageleni);
+        addPolyline(mMap,skoutari,peponia);
+        addPolyline(mMap,akamila,koumaria);
+        addPolyline(mMap,kkamila,koumaria);
+        addPolyline(mMap,koumaria,adelfiko);
+        addPolyline(mMap,ageleni,peponia);
+        addPolyline(mMap,peponia,adelfiko);
+    }
+
+    private void addPolyline(GoogleMap googleMap, LatLng start, LatLng end){
+        Polyline polyline = googleMap.addPolyline(new PolylineOptions()
+                .clickable(true)
+                .add(start,end));
+
+
     }
 }
